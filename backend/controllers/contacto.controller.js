83b51ac656e73566d95e34ac91d005065e7bf429
xLicht27 <138235405+xLicht27@users.noreply.github.com>
2026-04-11 14:50:58 -0500

    Arreglo de correos a funcion contacto

--- backend/controllers/contacto.controller.js
+++ backend/controllers/contacto.controller.js
@@ -4,12 +4,13 @@ const { sanitize } = require('../middlewares/sanitize');
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: parseInt(process.env.EMAIL_PORT),
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
 });
 
 /**
