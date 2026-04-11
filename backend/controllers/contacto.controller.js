50152a8a8cc8e8760a804d379d2b06aefa7bc85c
xLicht27 <138235405+xLicht27@users.noreply.github.com>
2026-04-11 15:09:36 -0500

    ajuste extra al sistema de correos

--- backend/controllers/contacto.controller.js
+++ backend/controllers/contacto.controller.js
@@ -5,7 +5,7 @@ const { sanitize } = require('../middlewares/sanitize');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
